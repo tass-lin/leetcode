2e4def7ea20a9780c489a3fe84053268cb47bef7
Tass <tass@TassdeMacBook-Pro.local>
2020-07-01 16:33:59 +0800

    add 0680 benchmark

--- 0680/0680-ValidPalindrome(II)_test.go
+++ 0680/0680-ValidPalindrome(II)_test.go
@@ -2,46 +2,46 @@ package main
 
 import "testing"
 
+type args struct {
+	s string
+}
+var tests = []struct {
+	name string
+	args args
+	want bool
+}{
+	{
+		"case1",
+		args{"aba"},
+		true,
+	},
+	{
+		"case2",
+		args{"abca"},
+		true,
+	},
+	{
+		"case3",
+		args{"tebbem"},
+		false,
+	},
+	{
+		"case4",
+		args{"abc"},
+		false,
+	},
+	{
+		"case5",
+		args{"abcbac"},
+		true,
+	},
+	{
+		"case6",
+		args{"aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"},
+		true,
+	},
+}
 func Test_validPalindrome(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			"case1",
-			args{"aba"},
-			true,
-		},
-		{
-			"case2",
-			args{"abca"},
-			true,
-		},
-		{
-			"case3",
-			args{"tebbem"},
-			false,
-		},
-		{
-			"case4",
-			args{"abc"},
-			false,
-		},
-		{
-			"case5",
-			args{"abcbac"},
-			true,
-		},
-		{
-			"case6",
-			args{"aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"},
-			true,
-		},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := validPalindrome(tt.args.s); got != tt.want {
@@ -52,50 +52,27 @@ func Test_validPalindrome(t *testing.T) {
 }
 
 func Test_validPalindrome1(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			"case1",
-			args{"aba"},
-			true,
-		},
-		{
-			"case2",
-			args{"abca"},
-			true,
-		},
-		{
-			"case3",
-			args{"tebbem"},
-			false,
-		},
-		{
-			"case4",
-			args{"abc"},
-			false,
-		},
-		{
-			"case5",
-			args{"abcbac"},
-			true,
-		},
-		{
-			"case6",
-			args{"aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"},
-			true,
-		},
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := validPalindrome1(tt.args.s); got != tt.want {
 				t.Errorf("validPalindrome1() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tests {
+			validPalindrome(tc.args.s)
+		}
+	}
+}
+
+func Benchmark_fn1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tests {
+			validPalindrome1(tc.args.s)
+		}
 	}
 }
